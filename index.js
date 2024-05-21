370793dd72aaf4e932857e0e42a188eebecf36e1
nickbennings <143043766+nickbennings@users.noreply.github.com>
2024-05-21 16:40:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,79 @@ app.post('/api/exercise/new-user', (req, res) => {
   });
 });
 
+// Route to get a list of all users
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(users);
+    }
+  });
+});
+
+// Route to add an exercise for a user
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { _id } = req.params;
+  const { description, duration, date } = req.body;
+
+  User.findById(_id, (err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    } else if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      const newExercise = new Exercise({
+        username: user.username,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date()
+      });
+
+      newExercise.save((err, exercise) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json({ username: user.username, ...exercise.toObject() });
+        }
+      });
+    }
+  });
+});
+
+// Route to retrieve a full exercise log of any user
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  const { from, to, limit } = req.query;
+
+  User.findById(_id, (err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    } else if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      let query = { username: user.username };
+
+      if (from || to) {
+        query.date = {};
+        if (from) query.date.$gte = new Date(from);
+        if (to) query.date.$lte = new Date(to);
+      }
+
+      Exercise.find(query)
+        .limit(limit ? parseInt(limit) : undefined)
+        .exec((err, exercises) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            const log = exercises.map(exercise => exercise.toObject());
+            res.json({ ...user.toObject(), log });
+          }
+        });
+    }
+  });
+});
+
 // Default route handler for root URL
 app.get('/', (req, res) => {
   res.send('Welcome to the Exercise Tracker App!');
